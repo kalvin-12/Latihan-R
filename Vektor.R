b9eb9d2899410b9ec0e39a9ecbe05a1821e39079
kalvin <VINIX@DEKSTOP-M61S7PN>
2024-02-09 23:04:39 +0700

    Vektor

---
+++ Vektor.R
@@ -0,0 +1,22 @@
+#Vektor 
+#Vektor dibuat dengan menggunakan fungsi c()(concatenate) 
+#seperti yang disajikan pada sintaks
+
+
+#y <- c(3,4,5,6,7,8)
+#x <- c(3,3,4,5,6)
+
+y # print vektor
+
+# Membuat vektor jumlah buah yang dibeli
+
+Jumlah <- c(Apel=3, Jeruk=5, Rambutan=6, Salak=7)
+
+# Print
+Jumlah
+
+
+Jumlah <- c(Apel=5, Jeruk=NA, Rambutan=6, Salak=7)
+
+
+is.na(Jumlah)
